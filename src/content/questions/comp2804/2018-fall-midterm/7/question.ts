38f96c92bc670a995531db598f4fe5fd7910b3b1
Ryan C. <87027981+ryangchung@users.noreply.github.com>
2024-10-27 16:54:08 -0400

    Fix latex symbols

--- src/content/questions/comp2804/2018-fall-midterm/7/question.ts
+++ src/content/questions/comp2804/2018-fall-midterm/7/question.ts
@@ -6,10 +6,10 @@ Consider 5-element subsets $\{x_1,x_2,x_3,x_4,x_5\}$ of the set $\{1,2,3,\dots,1
 		How many such subsets have the property that $x_3 = 7$?
 `;
 
-const label1 = "${6 choose 2} cdot {10 choose 2}$";
-const label2 = "${6 choose 2} cdot {9 choose 2}$";
-const label3 = "${7 choose 2} cdot {10 choose 2}$";
-const label4 = "${7 choose 2} cdot {9 choose 2}$";
+const label1 = "${6 choose 2} \cdot {10 choose 2}$";
+const label2 = "${6 choose 2} \cdot {9 choose 2}$";
+const label3 = "${7 choose 2} \cdot {10 choose 2}$";
+const label4 = "${7 choose 2} \cdot {9 choose 2}$";
 
 export const question: MultipleChoiceQuestion = {
   body: body,
